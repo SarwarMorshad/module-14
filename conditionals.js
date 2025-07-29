f62ec99a38247e9b8323d914978c5b399f42fcc6
sarwm-at-tu-chemnitz.de <sarwar.morshad@s2020.tu-chemnitz.de>
2025-07-29 08:59:04 +0200

    Practice

--- conditionals.js
+++ conditionals.js
@@ -7,3 +7,10 @@ if (condition) {
 if (3 < 10) {
   console.log("3 is less than 10");
 }
+
+var weight = 70;
+if (weight < 80) {
+  console.log("You are overweight");
+} else {
+  console.log("You are not overweight");
+}
