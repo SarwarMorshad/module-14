998a17167414536215650cdc95bd900a98747ee8
sarwm-at-tu-chemnitz.de <sarwar.morshad@s2020.tu-chemnitz.de>
2025-07-29 09:35:06 +0200

    Ternary operator

---
+++ ternary.js
@@ -0,0 +1,9 @@
+const age = 20;
+if (age >= 18) {
+  console.log("You can vote");
+} else {
+  console.log("You cannot vote");
+}
+
+// Ternary operator example
+console.log(age >= 18 ? "You can vote" : "You cannot vote");
