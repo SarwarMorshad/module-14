90f7419b311db1ee8c3a8cec9acb5c60e41c8fd8
sarwm-at-tu-chemnitz.de <sarwar.morshad@s2020.tu-chemnitz.de>
2025-07-29 10:13:18 +0200

    Ternary operator

--- ternary.js
+++ ternary.js
@@ -7,3 +7,22 @@ if (age >= 18) {
 
 // Ternary operator example
 console.log(age >= 18 ? "You can vote" : "You cannot vote");
+
+// Another example with ternary operator
+age >= 18 ? console.log("You can vote") : console.log("You cannot vote");
+
+const isLeader = true;
+const price = 500;
+
+if (isLeader === true) {
+  if (price > 1000) {
+    price = price / 2;
+  } else {
+    price = 0;
+  }
+} else {
+  price = price + 100;
+}
+
+// Ternary operator for the above logic
+price = isLeader === true ? (price > 1000 ? price / 2 : 0) : price + 100;
